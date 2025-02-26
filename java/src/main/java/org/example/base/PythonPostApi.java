aaea445e0996d66bdf03fb61766e16bff971f91a
siyuan.zhu <zhusiyuanhao@163.com>
2025-02-26 17:39:11 +0800

    ppp

--- java/src/main/java/org/example/base/PythonPostApi.java
+++ java/src/main/java/org/example/base/PythonPostApi.java
@@ -6,7 +6,7 @@ import cn.hutool.http.HttpUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
+import java.io.*;
 import java.util.Map;
 import java.util.Objects;
 import java.util.UUID;
@@ -25,9 +25,30 @@ public class PythonPostApi {
                 FileUtil.del(file);
             }
         }
-        HttpResponse httpResponse = HttpUtil.createPost("http://127.0.0.1:8000/get_voice").form(Map.of("tts_text",text,"path",mkdir.getAbsolutePath().replaceAll("\\\\", "\\\\\\\\"))).execute();
+        HttpResponse httpResponse = HttpUtil.createPost("http://127.0.0.1:8000/get_voice")
+                .form(Map.of("tts_text",text,"path",mkdir.getAbsolutePath().replaceAll("\\\\", "\\\\\\\\"))).execute();
         String body = httpResponse.body();
         log.info("run python end:"+body);
         return mkdir;
+    }
+    public File runPythonRemote(String text, String cosyUrl) {
+
+        HttpResponse httpResponse = HttpUtil.createPost(cosyUrl+"/get_voice_remote")
+                .form(Map.of("tts_text",text,"path","temp"))
+                .header("Accept", "audio/mpeg")
+                .execute();
+        InputStream body = httpResponse.bodyStream();
+        try (OutputStream outputStream = new FileOutputStream("output_voice.wav")) { // 保存到文件或实时处理
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = body.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+        }catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
     }
 }
--- java/src/test/java/org/example/VoiceGener.java
+++ java/src/test/java/org/example/VoiceGener.java
@@ -55,4 +55,31 @@ public class VoiceGener {
         voiceMerager.concat(file.getAbsolutePath(), path);
 
     }
+    @Test
+    public void test2() throws Exception {
+        String prompt = """
+                要知道福利可不是天天都有，主播下播之后，你直接到店里下单，就要回复原价了！
+                赶紧拍，赶紧冲，赶紧抢，赶紧囤喽。
+                如果你们拍完不想要了，都是可以 随时退，过期退，全额退，不收你一分钱手续费！
+                """;
+        //https://www.ai8.net/tutorial/2024/1231/596.html
+        prompt= """
+                        用开心的语气说<|endofprompt|> 哎呀妈呀，这福利可不是天天有啊！
+                        主播一下播，你直接到店里下单，嘿，那可就得回复原价啦！
+                        所以说，赶紧拍，赶紧冲，赶紧抢，赶紧囤喽。
+                        手慢无哦！
+                  
+                        但是万一您拍完之后突然觉得：
+                        哎？这个东西我不想要了咋办？
+                        别担心，咱们这儿可是全额退，过期退，随时退
+                        而且一分钱手续费都不收您的！
+                        怎么样，是不是很贴心呢？
+               
+                """;
+        File file = pythonPostApi.runPythonRemote(prompt, "http://192.168.1.3:8000");
+//        String name = "voice";
+//        String path = AppConfig.tempDir()+File.separator+ "gushi"+File.separator+name+".wav";
+//        voiceMerager.concat(file.getAbsolutePath(), path);
+
+    }
 }
